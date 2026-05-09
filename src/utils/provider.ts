07fa56ff3dc07dde7837c2bea098819f2fdfefb1
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-05-09 15:10:21 +0800

    refactor: update types

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -1,3 +1,5 @@
+import type { ResponsesOptions } from '@xsai-ext/responses'
+
 import type { CodexModels } from '../types/models'
 
 import { CODEX_BASE_URL } from './const'
@@ -7,11 +9,7 @@ export interface CreateCodexOptions {
 }
 
 export interface CreateCodexResult {
-  responses: (model: CodexModels | (string & {})) => {
-    apiKey: string
-    baseURL: string
-    model: string
-  }
+  responses: (model: CodexModels | (string & {})) => Pick<ResponsesOptions, 'apiKey' | 'baseURL' | 'headers' | 'model'>
 }
 
 export const createCodex = async (_options: CreateCodexOptions): Promise<CreateCodexResult> => {
